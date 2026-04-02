9017f495662142287666121bc2374325b2c3ff6e
fhan <aaron.han.1986@gmail.com>
2026-04-02 11:07:09 +0800

    fix scala checkstyle

--- hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/procedures/ArchiveCommitsProcedure.scala
+++ hudi-spark-datasource/hudi-spark/src/main/scala/org/apache/spark/sql/hudi/command/procedures/ArchiveCommitsProcedure.scala
@@ -26,6 +26,7 @@ import org.apache.spark.sql.Row
 import org.apache.spark.sql.types._
 
 import java.util.function.Supplier
+
 import scala.collection.JavaConverters._
 
 class ArchiveCommitsProcedure extends BaseProcedure
